6e294e91552239e666df5ff8016279b7b72e6de8
Pankaj Pandey <you@example.com>
2021-11-02 17:57:12 +0530

    fix releasebuild crash issue

--- Flutter-SampleApp/lib/main.dart
+++ Flutter-SampleApp/lib/main.dart
@@ -36,8 +36,8 @@ class _State extends State<MyApp> {
   /* To try the app with Enablex hosted service you need to set the kTry = true */
   static bool kTry = true;
   /*Use enablec portal to create your app and get these following credentials*/
-  static final String kAppId = "5ef5b31690ef80b4300b0bd2";
-  static final String kAppkey = "uJehyWaAu4uvyTupeJyJuHu6ygyYaGu2yzuq";
+  static final String kAppId = "app-id";
+  static final String kAppkey = "app-key";
   var header = (kTry)
       ? {
           "x-app-id": kAppId,
